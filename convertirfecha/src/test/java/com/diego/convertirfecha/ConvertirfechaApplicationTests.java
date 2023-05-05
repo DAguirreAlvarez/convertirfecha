3a535b99b0d6fe17819b9b99b2bec5252e38ebd6
Diego Aguirre <diego.a.aguirre.alvarez@gmail.com>
2023-05-05 09:48:45 -0400

    primer commit

---
+++ convertirfecha/src/test/java/com/diego/convertirfecha/ConvertirfechaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.convertirfecha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertirfechaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
